29ab2de239f58b516fbc0432967b88177489d6b7
malviles <137202863+malviles@users.noreply.github.com>
2023-08-18 18:19:29 -0600

    fix:cut_video

--- fotogrametrida/cut_video.py
+++ fotogrametrida/cut_video.py
@@ -1,25 +1,29 @@
 import cv2
-import time
+import datetime
 
-video_path = 'ruta_del_video.mp4'
-output_path = 'ruta_de_salida/'
+video_path = r"D:\manue\Documents\MATE\fotogrametrida\video\tree.mp4"
 
-cap = cv2.VideoCapture(video_path)
-frame_rate = cap.get(cv2.CAP_PROP_FPS)
+capture = cv2.VideoCapture(video_path)
+frame_rate = capture.get(cv2.CAP_PROP_FPS)
 interval = int(frame_rate) * 2  # Capturar un fotograma cada 2 segundos
 
 frame_count = 0
-while cap.isOpened():
-    ret, frame = cap.read()
+
+while capture.isOpened():
+    ret, frame = capture.read()
     if not ret:
+        print("No ret")
         break
     
     if frame_count % interval == 0:
-        timestamp = time.time()
-        output_filename = f'{output_path}captura_{int(timestamp)}.jpg'
-        cv2.imwrite(output_filename, frame)
-    
+        now = datetime.datetime.now()
+        formatted_date = now.strftime("%Y-%m-%d_%H-%M-%S")
+        print(formatted_date)
+        filename = f'captura_{str(formatted_date)}.jpg'
+        print('captura guardada')
+        cv2.imwrite(filename, frame)
+
     frame_count += 1
 
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
